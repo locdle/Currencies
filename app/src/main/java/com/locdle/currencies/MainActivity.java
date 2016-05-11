d9faa61921f2f50feb8709279fcfdf2f697650e6
Loc Le <ducloc1803@gmail.com>
2016-05-11 09:31:58 -0700

    unpack intent

--- app/src/main/java/com/locdle/currencies/MainActivity.java
+++ app/src/main/java/com/locdle/currencies/MainActivity.java
@@ -8,16 +8,24 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
     private Button mCalcuationButton;
     private TextView mConverterTextView;
     private EditText mAmountEditText;
     private Spinner mForSpinner, mHomeSpinner;
+    private String[] mCurrency;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> arrayList = (ArrayList<String>) getIntent().getSerializableExtra("key_arraylist");
+        Collections.sort(arrayList);
+        mCurrency = arrayList.toArray(new String [arrayList.size()]);
 
         mConverterTextView = (TextView) findViewById(R.id.txt_converted);
         mAmountEditText = (EditText) findViewById(R.id.edt_amount);
